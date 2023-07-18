fe19745c66b6c2c2aa5ee668b8505fedab8a4f87
jkabore1 <jkabore1@gmail.com>
2023-07-18 16:14:36 -0400

    first commit

--- src/redux/api/apiSlice.js
+++ src/redux/api/apiSlice.js
@@ -1,16 +1,18 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { userLoggedOut } from "../features/auth/authSlice";
+const devEnv = process.env.NODE_ENV !== "production";
+const { REACT_APP_DEV_API, REACT_APP_PROD_API } = process.env;
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000/api",
-  prepareHeaders: async (headers, {getState, endpoint}) => {
-      const token = getState()?.auth?.accessToken;
+  baseUrl: `${devEnv ? REACT_APP_DEV_API : REACT_APP_PROD_API}`,
+  prepareHeaders: async (headers, { getState, endpoint }) => {
+    const token = getState()?.auth?.accessToken;
 
-      if(token) {
-          headers.set('Authorization', `Bearer ${token}`);
-      }
-      return headers;
-  }
+    if (token) {
+      headers.set("Authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
 });
 
 export const apiSlice = createApi({
@@ -18,13 +20,13 @@ export const apiSlice = createApi({
   baseQuery: async (args, api, extraOptions) => {
     let result = await baseQuery(args, api, extraOptions);
 
-    if(result?.error?.status === 401) {
-        api.dispatch(userLoggedOut());
-        localStorage.clear();
+    if (result?.error?.status === 401) {
+      api.dispatch(userLoggedOut());
+      localStorage.clear();
     }
 
     return result;
-},
+  },
   mode: "cors",
   tagTypes: ["Jobs"],
   endpoints: (builder) => ({}),
