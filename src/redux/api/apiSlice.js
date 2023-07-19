3648a9ff82c7bb40bbb51fb8f199da51059dbe3d
jkabore1 <jkabore1@gmail.com>
2023-07-19 17:29:57 -0400

    first commit

--- src/redux/api/apiSlice.js
+++ src/redux/api/apiSlice.js
@@ -4,7 +4,7 @@ const devEnv = process.env.NODE_ENV !== "production";
 const { REACT_APP_DEV_API, REACT_APP_PROD_API } = process.env;
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: `${devEnv ? REACT_APP_DEV_API : REACT_APP_PROD_API}`,
+  baseUrl: `${REACT_APP_PROD_API}`,
   prepareHeaders: async (headers, { getState, endpoint }) => {
     const token = getState()?.auth?.accessToken;
     console.log("baseurl: ", REACT_APP_DEV_API, REACT_APP_PROD_API);
